30364abfa1660e7e746d90b4bcd2fe6c4faf9642
AhmedElhor7 <ahmedelhor446@gmail.com>
2024-10-10 09:58:28 +0300

    Updates

---
+++ assets/Products-DIhTD_Wt.js
@@ -0,0 +1,1 @@
+import{j as t,H as e}from"./index-Bnea2YKu.js";import{R as r}from"./RecentProducts-DgTNkPMd.js";function o(){return t.jsxs(t.Fragment,{children:[t.jsx(e,{children:t.jsx("title",{children:"Products"})}),t.jsx("h2",{className:"text-4xl font-bold text-green-600 flex items-center justify-center pb-6  2xl:mt-28",children:"Products"}),t.jsx(r,{})]})}export{o as default};
